47a991cd73fc49a8a1f7b91702d529c9728168e0
apsul319 <apsulpizio@gmail.com>
2022-02-18 23:58:48 -0500

    Added comments

--- sum.py
+++ sum.py
@@ -1,7 +1,7 @@
 from logging import exception
 
 
-def pairSum(x, upper, lower):
+def pairSum(x, upper, lower): # Generates a consecutive list of pairs for the specified number "x"
     combo = []
     for i in range(lower, upper+1, 1):
         for j in range(i+1, upper+1, 1):
@@ -13,7 +13,7 @@ def pairSum(x, upper, lower):
 
 validRange = False
 
-while not validRange:
+while not validRange: # Loop ensures user inputs a valid upper and lower limit without error
     try:
         up = int(input("Choose an upper limit (inclusive)\n"))
     except ValueError:
@@ -34,7 +34,7 @@ while not validRange:
 finishSet = False
 numSet = set()
 
-while not finishSet:
+while not finishSet: # Loop ensures user inputs as many numbers as requested into the set until specified otherwise by inputting "q"
     num = input("Type a number to put into the set. Enter \"q\" to quit\n")
     try:
         num = int(num)
@@ -49,6 +49,6 @@ while not finishSet:
         else:
             print("Number is not within set parameters")
 print()
-for val in sorted(numSet):
+for val in sorted(numSet): # For loop prints each unique number value entered into the set along with all unique pairs of numbers that were calculated within the specified range
     print(f"Pairs within {val}: {pairSum(val, up, low)}\n")
 
